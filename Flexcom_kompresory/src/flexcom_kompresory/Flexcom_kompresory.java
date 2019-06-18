8f98b32fce176f44c63a79bc764ae810c829564e
Kancl <Kancl@Kancl-HP>
2019-06-18 11:08:25 +0200

    Založení projektu

---
+++ Flexcom_kompresory/src/flexcom_kompresory/Flexcom_kompresory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flexcom_kompresory;
+
+/**
+ *
+ * @author Kancl
+ */
+public class Flexcom_kompresory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
